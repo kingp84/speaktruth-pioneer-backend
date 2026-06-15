f92b777fef9a0124c4135351384907aadeaa487c
Patrick King <patrick.e.king1958@gmail.com>
2026-06-15 17:48:25 -0500

    directory\admin.py update

--- directory/admin.py
+++ directory/admin.py
@@ -6,5 +6,5 @@ admin.site.register(Role)
 @admin.register(DirectoryEntry)
 class DirectoryEntryAdmin(admin.ModelAdmin):
     list_display = ('first_name', 'last_name', 'status')
-    list_filter = ('status', 'roles')
+    list_filter = ("status",)
     search_fields = ('first_name', 'last_name', 'phone', 'email')
