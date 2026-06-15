4a8ad9880b9a2ade0c62f09e30a41b728ec5a35e
Patrick King <patrick.e.king1958@gmail.com>
2026-06-15 12:53:58 -0500

    Fixed settings.py

--- speaktruth/settings.py
+++ speaktruth/settings.py
@@ -16,11 +16,18 @@ SECRET_KEY = os.environ.get("SECRET_KEY", "dev-secret-key")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG", "True") == "True"
 
+CSRF_TRUSTED_ORIGINS = [
+    "https://speaktruth.onrender.com",
+]
+
 ALLOWED_HOSTS = [
     "localhost",
     "127.0.0.1",
-    ".onrender.com",
+    "https://speaktruth.onrender.com",
 ]
+
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
 
 # Application definition
 INSTALLED_APPS = [
