568efc168a4745810179a4cbe3b8f55b8c40c000
DragonKing <patrick.e.king1958@gmail.com>
2026-08-09 21:59:51 -0500

    Add fields to admin.py

--- assignments/admin.py
+++ assignments/admin.py
@@ -3,8 +3,11 @@ from .models import Assignment
 
 @admin.register(Assignment)
 class AssignmentAdmin(admin.ModelAdmin):
-    list_display = ('date', 'service_type', 'role', 'person')
-    list_filter = ('service_type', 'role')
-    search_fields = ('person__first_name', 'person__last_name')
-    fields = ('date', 'service_type', 'role', 'person', 'notes')
+    list_display = ("date", "service_type", "role", "family", "person")
+    list_filter = ("service_type", "role")
+    search_fields = ("family", "notes", "person__first_name", "person__last_name")
+
+    # This controls what fields appear in the admin form
+    fields = ("date", "service_type", "role", "family", "person", "notes")
+
 
